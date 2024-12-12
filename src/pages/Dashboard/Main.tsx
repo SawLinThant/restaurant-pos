487d0a53f6c8786fbfcba07ea786cc82869e6e37
Saw Lin Thant <115479032+SawLinThant@users.noreply.github.com>
2024-12-12 21:42:24 +0700

    merge develop

--- src/pages/Dashboard/Main.tsx
+++ src/pages/Dashboard/Main.tsx
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 import MenuDetail from "@/components/Dashboard/Detail/menu/[menuId]";
->>>>>>> feature/dashboard
 import { MenuForm, StaffForm } from "@/components/Dashboard/Form";
 import { Menu, OrderList, Overview, Staff } from "@/components/Dashboard/MainContent";
 import Sidebar from "@/components/Dashboard/Sidebar";
@@ -17,10 +14,7 @@ const MainScreen = () => {
                 <Route path="*" element={<Overview/>}/>
                 <Route path="staff" element={<Staff/>}/>
                 <Route path="menu" element={<Menu/>}/>
-<<<<<<< HEAD
-=======
                 <Route path="menu/:menuId" element={<MenuDetail/>}/>
->>>>>>> feature/dashboard
                 <Route path="menu/createmenu" element={<MenuForm/>}/>
                 <Route path="staff/createstaff" element={<StaffForm/>}/>
                 <Route path="orderlist" element={<OrderList/>}/>
