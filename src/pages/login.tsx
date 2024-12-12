11cac05b3db9436a3ca77175165c048a6e432a0c
Saw Lin Thant <115479032+SawLinThant@users.noreply.github.com>
2024-12-12 22:51:08 +0700

    Rename Login.tsx and Register.tsx to lowercase

---
+++ src/pages/login.tsx
@@ -0,0 +1,7 @@
+import LoginForm from "@/components/account/login";
+
+const Login = () => {
+  return <div><LoginForm/></div>;
+};
+
+export default Login;
---
+++ src/pages/register.tsx
@@ -0,0 +1,9 @@
+import RegisterForm from "@/components/account/register";
+
+const Register = () => {
+  return <div>
+    <RegisterForm/>
+  </div>;
+};
+
+export default Register;
